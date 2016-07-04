de09c7991248c48b937ee23326a155da440985e1
Katarina <klingat@me.com>
2016-07-04 19:53:04 +0000

    Added variables to coding-style.

---
+++ coding-style.js
@@ -0,0 +1,5 @@
+var age = 22; // <- don't forget semicolon at end of each statement
+//     ^ ^ having white space around = sign is good :)
+// it makes the code more readable
+
+var name = "Kasia";

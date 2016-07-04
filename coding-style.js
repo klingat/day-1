016927b1ac57ef2582fd8189b495f8874eb58540
Katarina <klingat@me.com>
2016-07-04 20:45:51 +0000

    Added notes about function to coding-style

--- coding-style.js
+++ coding-style.js
@@ -7,8 +7,31 @@ var name = "Kasia";
 
 var num = 10;
 
+
 // Curly brackets
 if (num > 0) { // <- Always have {} even if you have one statement.
     console.log('the number is greater than zero')
-};
+}
+
+
+// Functions
+
+/*
+These 3 different ways of declaring functions are not 100% equivalenet,
+we will see the differences between them.
+
+Use the first version.
+*/
+
+function doSomething(sentence) { // <- Use this type of function for coding
+    // .. code goes here
+}
+
+var doSomething = function(sentence) {
+    // .. code goes here
+}
+
+var doSomething = function doSomething(sentence) {
+    // .. code goes here
+}
 
